435d3f4b28f97ecac2ff1fe5f9b4f6309e73bd4a
Anton Kulchytskyi <antek1809@gmail.com>
2022-10-17 00:23:06 +0200

    url to server changed

--- src/App.js
+++ src/App.js
@@ -9,9 +9,10 @@ function App() {
 }
 
 function callApi() {
-    fetch('http://localhost:3001/details', { method: 'GET' })
+    fetch('http://simple-node-server-try.herokuapp.com', { method: 'GET' })
         .then(data => data.json())
         .then(json => alert(JSON.stringify(json)))
 }
 
 export default App;
+
