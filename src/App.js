6e22c5f1b77166e13458a8255c4559546ee9dc9c
Anton Kulchytskyi <antek1809@gmail.com>
2022-10-17 00:33:22 +0200

    deleted unnecessary code

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
 }
 
 function callApi() {
-    fetch('http://simple-node-server-try.herokuapp.com', { method: 'GET' })
+    fetch('http://localhost:3001/details', { method: 'GET' })
         .then(data => data.json())
         .then(json => alert(JSON.stringify(json)))
 }
